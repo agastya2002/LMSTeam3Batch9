ba5c23f5cd42fd875730a96884f8b59ae0ba63cd
soumilkamat <79736288+soumilkamat@users.noreply.github.com>
2023-09-07 15:42:37 +0530

    Add files via upload
    Added item_master populate script

---
+++ ItemMasterPopulate.sql
@@ -0,0 +1,5 @@
+insert into item_master values 
+('I0001','Issued','Table','Wood', 'Furniture', 1000),
+('I0002','Issued','Plates','Glass', 'Crockery', 100),
+('I0003','Issued','Scissors','Plastic', 'Stationery', 50),
+('I0004','Issued','Chair','Wood', 'Furniture', 500)
