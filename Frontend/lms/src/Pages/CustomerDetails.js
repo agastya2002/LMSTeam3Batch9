c37474291fb9c345a605dac6f4a670c4d84f50fa
Hardik Sharma <hardik00051@gmail.com>
2023-09-11 00:06:51 +0530

    feat: added frontend form validation

--- Frontend/lms/src/Pages/CustomerDetails.js
+++ Frontend/lms/src/Pages/CustomerDetails.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import '../Styles/CustomerDetails.css'
+import { useState } from 'react';
 
 const CustomerDetails=()=>{
+
+    const [eID,setEID]=useState("");
+    const [eName,setEName]=useState("");
+    const [department,setDepartment]=useState("");
+    const [gender,setGender]=useState("");
+    const [designation,setDesignation]=useState("");
+    const [dob,setDob]=useState("");
+    const [doj,setDoj]=useState("");
+    let isDataValid=true;
+
+    const validateEntries=()=>{
+        //check employee ID
+        if (!eID.match(/[A-Z]{1}[0-9]/)){
+            alert("Invalid employee ID format!");
+            isDataValid=false;
+        }
+        if(eName.length===0){
+            alert("Please enter employee name!");
+            isDataValid=false;
+        }
+        if(!eName.match(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g)){
+            alert("Employee name is invalid!\nUse of special characters and numbers is not allowed")
+            isDataValid=false;
+        }
+        if(dob.length===0){
+            alert("Please enter date of birth!");
+            isDataValid=false;
+        }
+        if(doj.length===0){
+            alert("Please enter date of joining!");
+            isDataValid=false;
+        }
+        console.log(isDataValid);
+    }
     return (
         <div>
             <div>
@@ -12,18 +47,18 @@ const CustomerDetails=()=>{
                 <div className='employee_details'>
                     <div className='employee_detail'>
                         <span className='employee_field'>Employee id</span>
-                        <input type="text" id="eid" />
+                        <input type="text" id="eid" onChange={(e) => setEID(e.target.value)} />
                     </div>
                     <div className='employee_detail'>
                         <span className='employee_field'>Employee Name</span>
-                        <input type="text" id="ename" />
+                        <input type="text" id="ename" onChange={(e) => setEName(e.target.value)}/>
                     </div>
                 </div>
                 <div className='employee_details'>
                     <div className='employee_detail'>
                     <span className='employee_field'>Department</span>
                         <div>
-                            <select name="department" id="department"> 
+                            <select name="department" id="department" onChange={(e) => setDepartment(e.target.value)}> 
                                 <option value="it">IT</option> 
                                 <option value="finance">Finance</option> 
                                 <option value="sales">Sales</option> 
@@ -34,7 +69,7 @@ const CustomerDetails=()=>{
                     <div className='employee_detail'>
                     <span className='employee_field'>Gender</span>
                         <div>
-                            <select name="gender" id="gender"> 
+                            <select name="gender" id="gender" onChange={(e) => setGender(e.target.value)}> 
                                 <option value="male">Male</option> 
                                 <option value="female">Female</option>
                             </select>    
@@ -43,7 +78,7 @@ const CustomerDetails=()=>{
                     <div className='employee_detail'>
                         <span className='employee_field'>Designation</span>
                         <div>
-                            <select name="designation" id="designation"> 
+                            <select name="designation" id="designation" onChange={(e) => setDesignation(e.target.value)}> 
                                 <option value="manager">Manager</option> 
                                 <option value="ca">CA</option> 
                                 <option value="dgm">DGM</option> 
@@ -55,15 +90,15 @@ const CustomerDetails=()=>{
                 <div className='employee_details'>
                     <div className='employee_detail'>
                     <span className='employee_field'>Date Of Birth</span>
-                        <div><input type="date" id="dob" /></div>
+                        <div><input type="date" id="dob" onChange={(e) => setDob(e.target.value)}/></div>
                     </div>
                     <div className='employee_detail'>
                     <span className='employee_field'>Date Of Joining</span>
-                        <div><input type="date" id="doj" /></div>
+                        <div><input type="date" id="doj" onChange={(e) => setDoj(e.target.value)} /></div>
                     </div>
                 </div>
                 <div>
-                    <button type="submit">Add Data</button>
+                    <button type="submit" onClick={()=>validateEntries()}>Add Data</button>
                 </div>
             
             </div>
